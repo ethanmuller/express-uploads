059f2a124265175f12812fa291c1dbabad3a138c
Ethan Muller <hi@emu.media>
2023-07-27 11:16:02 -0400

    accept base64 drawings from client

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const app = express();
+const ba64 = require('ba64')
 const port = 3000;
+
+app.use(cors())
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 // Require the upload middleware
 const upload = require('./upload');
@@ -11,8 +19,9 @@ app.get('/', (req, res) => {
 });
 
 // Set up a route for file uploads
-app.post('/upload', upload.single('file'), (req, res) => {
+app.post('/upload', (req, res) => {
   // Handle the uploaded file
+  ba64.writeImageSync('uploads/' + Date.now(), req.body.base64image)
   res.json({ message: 'File uploaded successfully!' });
 });
 
